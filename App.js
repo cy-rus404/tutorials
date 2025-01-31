229ee0a51c028834cda54010676d7ad656df05d6
cy-rus404 <sduisaac@yahoo.com>
2025-01-31 21:34:29 +0000

    new

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { ActivityIndicator, View,Text } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const LoadingScreen = () =>(
+  <View 
+   style={{flex:1,justifyContent:'center',alignItems:'center'}}
+  >
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <Text> Loading Screen Please wait .....</Text>
+
+<ActivityIndicator size='large' color= "#0000ff"/>
+
+  </View>
+)
+
+export default LoadingScreen;  //exporting the component
